706f99e27e6db997aef71627d40bd705b0d91e47
YURI <yuri.dev.contact@gmail.com>
2025-12-14 22:25:04 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+
+<body>
+  <p>
+    <?php
+      $vegetables = [
+        'name'=>'onion',
+        'price'=>200,
+        'weight'=>160
+      ];
+
+      print_r($vegetables);
+    ?>
+  </p>
+</body>
+
+</html>
